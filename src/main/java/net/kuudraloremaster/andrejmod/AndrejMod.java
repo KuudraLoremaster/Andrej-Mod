f4e9d86a08b353e48f6ffc147e1f9f4772fbd67b
KuudraLoremaster <zahranomar2010@gmail.com>
2024-05-13 22:45:16 +0300

    item :)

--- src/main/java/net/kuudraloremaster/andrejmod/AndrejMod.java
+++ src/main/java/net/kuudraloremaster/andrejmod/AndrejMod.java
@@ -1,8 +1,11 @@
 package net.kuudraloremaster.andrejmod;
 
 import com.mojang.logging.LogUtils;
+import net.kuudraloremaster.andrejmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -25,12 +28,12 @@ public class    AndrejMod
     public AndrejMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
+        ModItems.register(modEventBus);
         modEventBus.addListener(this::commonSetup);
 
 
         MinecraftForge.EVENT_BUS.register(this);
-
+        modEventBus.addListener(this::addCreative);
 
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
 //        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
@@ -39,6 +42,12 @@ public class    AndrejMod
     private void commonSetup(final FMLCommonSetupEvent event)
     {
 
+    }
+
+    private  void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAPPHIRE);
+        }
     }
 
 
