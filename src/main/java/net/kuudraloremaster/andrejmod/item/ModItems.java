7f41e66a7b10d54c9b78fefb28b43c5a51c21ef2
KuudraLoremaster <zahranomar2010@gmail.com>
2024-05-14 14:18:01 +0300

    whole lotta shit (pixelated face irl actually does something)

--- src/main/java/net/kuudraloremaster/andrejmod/item/ModItems.java
+++ src/main/java/net/kuudraloremaster/andrejmod/item/ModItems.java
@@ -1,6 +1,8 @@
 package net.kuudraloremaster.andrejmod.item;
 
 import net.kuudraloremaster.andrejmod.AndrejMod;
+import net.kuudraloremaster.andrejmod.item.custom.MetalDetectorItem;
+import net.kuudraloremaster.andrejmod.item.custom.PixelatedFaceItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,7 +17,9 @@ public class ModItems {
     public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
             ()-> new Item(new Item.Properties()));
     public static final RegistryObject<Item> PIXEL = ITEMS.register("pixel",
-            ()-> new Item(new Item.Properties()));
+            ()-> new PixelatedFaceItem(new Item.Properties()));
+    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
+            ()-> new MetalDetectorItem(new Item.Properties().durability(100)));
     public static void register(IEventBus eventbus) {
         ITEMS.register(eventbus);
     }
---
+++ src/main/java/net/kuudraloremaster/andrejmod/item/custom/PixelatedFaceItem.java
@@ -0,0 +1,32 @@
+package net.kuudraloremaster.andrejmod.item.custom;
+
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+
+import java.util.Objects;
+
+public class PixelatedFaceItem extends Item {
+    public PixelatedFaceItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        Player player = pContext.getPlayer();
+        player.sendSystemMessage(Component.literal("pixelated face is schizophrenic"));
+        return InteractionResult.SUCCESS;
+    }
+
+}
